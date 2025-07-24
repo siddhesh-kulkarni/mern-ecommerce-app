ec75fb1633154608e1281557a9a32a32f310c219
siddhesh-kulkarni <siddheshsanjaykulkarni1111@gmail.com>
2025-07-25 00:13:50 +0530

    Added header and search bar with react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import "./App.css";
-import Header from "./Components/Header";
+import Header from "./Components/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Header/>
+      <Routes>
+        <Route path={'/'} exact={true} element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
