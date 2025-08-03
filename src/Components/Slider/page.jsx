e48203cd3add4055b1a3f92a42fb7394c401409e
siddhesh-kulkarni <siddheshsanjaykulkarni1111@gmail.com>
2025-08-03 21:56:14 +0530

    Banner images for home screen

---
+++ src/Components/Slider/page.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+
+const Slider = () => {
+  return (
+    <>
+      <div className="homeSlider py-4">
+        <div className="container">
+          <Swiper
+            navigation={true}
+            modules={[Navigation]}
+            className="sliderHome"
+            spaceBetween={10}
+          >
+            <SwiperSlide>
+              <div className="item rounded-[20px] overflow-hidden">
+                <img
+                  src="images/Slider/slider1.jpg"
+                  className="w-full"
+                  alt="Banner Images"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item rounded-[20px] overflow-hidden">
+                <img
+                  src="images/Slider/slider2.jpg"
+                  className="w-full"
+                  alt="Banner Images"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item rounded-[20px] overflow-hidden">
+                <img
+                  src="images/Slider/slider3.jpg"
+                  className="w-full"
+                  alt="Banner Images"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item rounded-[20px] overflow-hidden">
+                <img
+                  src="images/Slider/slider4.jpg"
+                  className="w-full"
+                  alt="Banner Images"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item rounded-[20px] overflow-hidden">
+                <img
+                  src="images/Slider/slider5.jpg"
+                  className="w-full"
+                  alt="Banner Images"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item rounded-[20px] overflow-hidden">
+                <img
+                  src="images/Slider/slider6.jpg"
+                  className="w-full"
+                  alt="Banner Images"
+                />
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Slider;
